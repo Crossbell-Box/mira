382bb30676e7a6942b69516a35598b10cace26b8
Songkeys <songv587@gmail.com>
2023-01-31 16:52:13 +0800

    fix(mira): ssr

--- apps/mira/pages/_document.tsx
+++ apps/mira/pages/_document.tsx
@@ -1,10 +1,31 @@
-import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import { emotionCache } from "@/components/Providers/ThemeProvider";
 
-const getInitialProps = createGetInitialProps();
+const stylesServer = createStylesServer(emotionCache);
 
 export default class _Document extends Document {
-	static getInitialProps = getInitialProps;
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return {
+			...initialProps,
+			styles: [
+				initialProps.styles,
+				<ServerStyles
+					html={initialProps.html}
+					server={stylesServer}
+					key="styles"
+				/>,
+			],
+		};
+	}
 
 	render() {
 		return (
